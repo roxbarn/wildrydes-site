c9070605d8b4cbc77dfdae6d61133bd202a83f8f
roxbarn <roxanne_barney@hotmail.com>
2019-11-27 11:06:13 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_yYplCIo9s', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3ccenkb0d95k7bmpguc8ku4ri', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
